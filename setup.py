3572a100a80219d30f03ddfa1d1b7800528c9fd8
CyperRangers <108158123+CyperRangers@users.noreply.github.com>
2022-06-25 01:13:25 +0200

    Update setup.py
    Change Credentials

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ except:
 setup(
     name = "PythonPi",
     version = '1.0.2',
-    author = 'Pradipta Bora',
-    author_email = 'pradd@outlook.com',
+    author = 'CyperRangers',
+    author_email = 'cyper@tutamail.com',
     description = "Get the Value of Pi upto as many decimal places as needed",
     license = "MIT",
     keywords = "pi maths",
-    url = "https://github.com/geekpradd/PythonPi",
+    url = "https://github.com/CyperRangers/CyperRange2",
     py_modules = ['PythonPi'],
     entry_points = {
     'console_scripts': ['pythonpi = PythonPi:shell']
